bced451805ffa74b5d8f827992ca7074e715157c
Ethan <30396562+etrant1@users.noreply.github.com>
2023-12-03 21:28:34 -0500

    Move template homepage to parent folder

--- lib/auth/pages/auth_gate.dart
+++ lib/auth/pages/auth_gate.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:quickbites/auth/pages/login_or_register.dart';
 
-import 'home_page.dart';
+import '../../home_page.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
