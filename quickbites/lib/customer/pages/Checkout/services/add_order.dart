dfd9cfe83654a7b3c9f988d72d605493cdba39dd
jayl25 <jaylimbasia25@gmail.com>
2023-12-08 11:31:34 -0500

    Updated Firebase Functionality

--- quickbites/lib/customer/pages/Checkout/services/add_order.dart
+++ quickbites/lib/customer/pages/Checkout/services/add_order.dart
@@ -1,21 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:quickbites/customer/order_tracking/models/order.dart';
+import 'package:flutter/material.dart';
 
 class OrderService {
-  FirebaseAuth _auth = FirebaseAuth.instance;
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  void createOrder() async {
-    ItemOrder newOrder = const ItemOrder(
-      orderitems: ["Supreme Chalupa"],
-      orderTotal: 5.60,
-      restaurantName: "Taco Bell",
-      restaurantAddress: "429 Ponce De Leon Ave NE",
-    );
+  Future<void> createOrder(BuildContext context, var order) async {
     await FirebaseFirestore.instance
         .collection('orders')
         .doc(FirebaseAuth.instance.currentUser!.uid)
-        .set(newOrder.toMap());
+        .set(
+      {
+        "restaurantName": order.items.restaurantName,
+        "latitude": order.items.latitude,
+        "longitude": order.items.longitude,
+        "address": order.items.address,
+        "orderTotal": order.items.orderTotal,
+        "items": order.items
+      },
+    );
   }
 }
